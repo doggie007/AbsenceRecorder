d4b5eba4726397301bfa3830fba5a08d7732d25f
James Tcheng <james.tcheng1234@gmail.com>
2022-01-23 10:22:28 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Tcheng, James (BJH) on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
