1cfbafca994e1c24f7c90151f41b29281ffa9572
James Tcheng <james.tcheng1234@gmail.com>
2022-01-23 10:38:12 +0000

    Added model for division and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,16 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Tcheng, James (BJH) on 23/01/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    init(code:String){
+        self.code = code
+    }
+}
